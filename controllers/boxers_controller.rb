c8ce2b9ffb61b563a031f52c4be1d05245f479f7
Adams1991 <adams121@hotmail.co.uk>
2018-06-17 21:28:36 +0100

    boxers/new erb initial setup complete

--- controllers/boxers_controller.rb
+++ controllers/boxers_controller.rb
@@ -3,11 +3,18 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/boxer.rb' )
 also_reload( '../models/*' )
 
+# INDEX
 get '/boxers' do
   @boxers = Boxer.all()
   erb ( :"boxers/index" )
 end
 
+# NEW
+get('/boxers/new') do
+  erb( :"boxers/new" )
+end
+
+#SHOW
 get '/boxers/:id' do
   @boxer = Boxer.find(params['id'].to_i)
   erb(:"boxers/show")
