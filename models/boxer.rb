b9a6ebf725e46ab3d54cf6f82354e9cd7a8b59ae
Adams1991 <adams121@hotmail.co.uk>
2018-06-17 19:07:31 +0100

    changes made to boxer model

--- models/boxer.rb
+++ models/boxer.rb
@@ -2,11 +2,12 @@ require_relative('../db/sql_runner')
 
 class Boxer
 
-attr_reader(:id, :name)
+attr_reader(:id, :first_name, :last_name)
 
   def initialize(options)
     @id = options['id'].to_i
-    @name = options['name']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
   end
 
 end
--- models/specs/boxer_spec.rb
+++ models/specs/boxer_spec.rb
@@ -4,14 +4,19 @@ require_relative("../boxer")
 class TestBoxer < MiniTest::Test
 
   def setup
-    options = {"id" => 1, "name" => "John Adams"}
+    options = {"id" => 1, "first_name" => "John", "last_name" => "Adams"}
 
     @boxer = Boxer.new(options)
   end
 
-  def test_name()
-    result = @boxer.name()
-    assert_equal("John Adams", result)
+  def test_first_name()
+    result = @boxer.first_name()
+    assert_equal("John", result)
+  end
+
+  def test_last_name()
+    result = @boxer.last_name()
+    assert_equal("Adams", result)
   end
 
   def test_id()
