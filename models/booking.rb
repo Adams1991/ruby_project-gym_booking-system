d4350c3e41b3605efa77b92883ffaeb3164b318b
Adams1991 <adams121@hotmail.co.uk>
2018-06-17 19:38:07 +0100

    booking model find all and find by id working

--- models/booking.rb
+++ models/booking.rb
@@ -6,8 +6,8 @@ attr_reader(:id, :boxer_id, :fitness_class_id)
 
   def initialize(options)
     @id = options['id'].to_i
-    @boxer_id = options['boxer_id']
-    @fitness_class_id = options['fitness_class_id']
+    @boxer_id = options['boxer_id'].to_i
+    @fitness_class_id = options['fitness_class_id'].to_i
   end
 
   def save()
@@ -29,6 +29,20 @@ attr_reader(:id, :boxer_id, :fitness_class_id)
   def self.delete_all
     sql = "DELETE FROM bookings"
     SqlRunner.run( sql )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM bookings"
+    results = SqlRunner.run( sql )
+    return results.map { |hash| Booking.new( hash ) }
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM bookings
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Booking.new( results.first )
   end
 
 
