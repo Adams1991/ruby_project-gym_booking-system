577ef2d007019b4937eabf4869d0189defb6c1d5
Adams1991 <adams121@hotmail.co.uk>
2018-06-18 08:22:06 +0100

    fitness cclass restful routes partially setup

--- controllers/fitness_classes_controller.rb
+++ controllers/fitness_classes_controller.rb
@@ -3,12 +3,27 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/fitness_class.rb' )
 also_reload( '../models/*' )
 
-get '/victims' do
-  @victims = Victim.all()
-  erb ( :"victims/index" )
+
+# INDEX
+get('/fitness_class') do
+  @fitness_classes = FitnessClass.all()
+  erb ( :"fitness_classes/index" )
 end
 
-get '/victims/:id' do
-  @victim = Victim.find(params['id'].to_i)
-  erb( :"victims/show" )
+# NEW
+get('/fitness_classes/new') do
+  erb( :"fitness_classes/new" )
+end
+
+#SHOW
+get('/fitness_classes/:id') do
+  @fitness_class = Boxer.find(params['id'].to_i)
+  erb(:"fitness_classes/show")
+end
+
+#CREATE
+post('/fitness_classes') do
+  @fitness_class = Boxer.new(params)
+  @fitness_class.save()
+  erb ( :"fitness_classes/create" )
 end
