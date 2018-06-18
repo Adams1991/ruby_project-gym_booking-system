083cc8faef6e2f0b03edff7bd32d8f4f64b9f742
Adams1991 <adams121@hotmail.co.uk>
2018-06-18 12:07:11 +0100

    create booking working on app

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -12,42 +12,38 @@ get('/bookings') do
 end
 
 # NEW
-get('/boxers/new') do
-  erb( :"boxers/new" )
-end
-
-#SHOW
-get('/boxers/:id') do
-  @boxer = Boxer.find(params['id'].to_i)
-  erb(:"boxers/show")
+get('/bookings/new') do
+  @boxers = Boxer.all()
+  @fitness_classes = FitnessClass.all()
+  erb( :"bookings/new" )
 end
 
 #CREATE
-post('/boxers') do
-  @boxer = Boxer.new(params)
-  @boxer.save()
-  erb ( :"boxers/create" )
+post('/bookings') do
+  @booking = Booking.new(params)
+  @booking.save()
+  erb ( :"bookings/create" )
 end
 
-# DELETE
-get('/boxers/:id/delete') do
-  boxer = Boxer.find(params[:id].to_i)
-  boxer.delete()
-  erb( :"boxers/destroy" )
-end
-
-# EDIT
-get('/boxers/:id/edit') do
-  @boxer = Boxer.find(params[:id].to_i)
-  erb( :"boxers/edit" )
-end
-
-#UPDATE
-post('/boxers/:id') do
-  @boxer = Boxer.new(params)
-  @boxer.update()
-  erb( :"boxers/update" )
-end
+# # DELETE
+# get('//:id/delete') do
+#   boxer = Boxer.find(params[:id].to_i)
+#   boxer.delete()
+#   erb( :"boxers/destroy" )
+# end
+#
+# # EDIT
+# get('/boxers/:id/edit') do
+#   @boxer = Boxer.find(params[:id].to_i)
+#   erb( :"boxers/edit" )
+# end
+#
+# #UPDATE
+# post('/boxers/:id') do
+#   @boxer = Boxer.new(params)
+#   @boxer.update()
+#   erb( :"boxers/update" )
+# end
 
 # #ADD TO CLASS
 # post('boxers/:id/add_to_class') do
