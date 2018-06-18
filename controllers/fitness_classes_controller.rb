72e08f14ea4ad0132c1cbb51d9b1b925c642b9d8
Adams1991 <adams121@hotmail.co.uk>
2018-06-18 08:36:30 +0100

    index, create and new restful routes skeleton setup for fitness classes

--- controllers/fitness_classes_controller.rb
+++ controllers/fitness_classes_controller.rb
@@ -5,7 +5,7 @@ also_reload( '../models/*' )
 
 
 # INDEX
-get('/fitness_class') do
+get('/fitness_classes') do
   @fitness_classes = FitnessClass.all()
   erb ( :"fitness_classes/index" )
 end
@@ -17,13 +17,13 @@ end
 
 #SHOW
 get('/fitness_classes/:id') do
-  @fitness_class = Boxer.find(params['id'].to_i)
+  @fitness_class = FitnessClass.find(params['id'].to_i)
   erb(:"fitness_classes/show")
 end
 
 #CREATE
 post('/fitness_classes') do
-  @fitness_class = Boxer.new(params)
+  @fitness_class = FitnessClass.new(params)
   @fitness_class.save()
   erb ( :"fitness_classes/create" )
 end
