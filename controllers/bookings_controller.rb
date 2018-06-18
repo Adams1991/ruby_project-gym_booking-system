d73e6453a866300ff76c459bcc42629fb97d1343
Adams1991 <adams121@hotmail.co.uk>
2018-06-18 11:59:12 +0100

    bookingrestful routes started

---
+++ controllers/bookings_controller.rb
@@ -0,0 +1,57 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/boxer.rb' )
+require_relative( '../models/fitness_class.rb' )
+require_relative( '../models/booking.rb' )
+also_reload( '../models/*' )
+
+# INDEX
+get('/bookings') do
+  @bookings = Booking.all()
+  erb ( :"bookings/index" )
+end
+
+# NEW
+get('/boxers/new') do
+  erb( :"boxers/new" )
+end
+
+#SHOW
+get('/boxers/:id') do
+  @boxer = Boxer.find(params['id'].to_i)
+  erb(:"boxers/show")
+end
+
+#CREATE
+post('/boxers') do
+  @boxer = Boxer.new(params)
+  @boxer.save()
+  erb ( :"boxers/create" )
+end
+
+# DELETE
+get('/boxers/:id/delete') do
+  boxer = Boxer.find(params[:id].to_i)
+  boxer.delete()
+  erb( :"boxers/destroy" )
+end
+
+# EDIT
+get('/boxers/:id/edit') do
+  @boxer = Boxer.find(params[:id].to_i)
+  erb( :"boxers/edit" )
+end
+
+#UPDATE
+post('/boxers/:id') do
+  @boxer = Boxer.new(params)
+  @boxer.update()
+  erb( :"boxers/update" )
+end
+
+# #ADD TO CLASS
+# post('boxers/:id/add_to_class') do
+#   @fitness_classes = FitnessClass.all()
+#   @boxer = Boxer.find(params[:id].to_i)
+#   @boxer.add_fitness_class(params)
+# end
