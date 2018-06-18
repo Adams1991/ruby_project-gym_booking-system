ee802c7aabee3ecb7d4d128079c38b51cb1fa470
Adams1991 <adams121@hotmail.co.uk>
2018-06-18 12:51:26 +0100

    remove booking function working

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -25,29 +25,9 @@ post('/bookings') do
   erb ( :"bookings/create" )
 end
 
-# # DELETE
-# get('//:id/delete') do
-#   boxer = Boxer.find(params[:id].to_i)
-#   boxer.delete()
-#   erb( :"boxers/destroy" )
-# end
-#
-# # EDIT
-# get('/boxers/:id/edit') do
-#   @boxer = Boxer.find(params[:id].to_i)
-#   erb( :"boxers/edit" )
-# end
-#
-# #UPDATE
-# post('/boxers/:id') do
-#   @boxer = Boxer.new(params)
-#   @boxer.update()
-#   erb( :"boxers/update" )
-# end
-
-# #ADD TO CLASS
-# post('boxers/:id/add_to_class') do
-#   @fitness_classes = FitnessClass.all()
-#   @boxer = Boxer.find(params[:id].to_i)
-#   @boxer.add_fitness_class(params)
-# end
+# DELETE
+get('/bookings/:id/delete') do
+  booking = Booking.find(params[:id].to_i)
+  booking.delete()
+  erb( :"boxers/destroy" )
+end
