199555825aef84403871cb99f89c86a4410ad80d
Adams1991 <adams121@hotmail.co.uk>
2018-06-19 14:04:19 +0100

    trainer spec setup complete

---
+++ models/specs/trainer_spec.rb
@@ -0,0 +1,39 @@
+require("minitest/autorun")
+require_relative("../trainer")
+
+class TestTrainer < MiniTest::Test
+
+  def setup
+    options = {"id" => 1, "first_name" => "Baba", "last_name" => "Gouch", "premium_trainer" => true}
+
+    @trainer= Trainer.new(options)
+  end
+
+  def test_first_name()
+    result = @trainer.first_name()
+    assert_equal("Baba", result)
+  end
+
+  def test_last_name()
+    result = @trainer.last_name()
+    assert_equal("Gouch", result)
+  end
+
+  def test_id()
+    result = @trainer.id()
+    assert_equal(1, result)
+  end
+
+  def test_premium_member()
+    result = @trainer.premium_member()
+    assert_equal(true, result)
+  end
+
+  def test_full_name
+    result = @trainer.full_name()
+    assert_equal("Baba Gouch", result)
+  end
+
+
+
+end
