c3753c2a92a4dd011ca23b6bf96fedcede9cfcc2
Adams1991 <adams121@hotmail.co.uk>
2018-06-20 10:52:52 +0100

    spec for fitness class updated and drop down not show classes not available on booking

--- models/specs/fitness_class_spec.rb
+++ models/specs/fitness_class_spec.rb
@@ -4,7 +4,7 @@ require_relative("../fitness_class")
 class TestFitnessClass < MiniTest::Test
 
   def setup
-    options = {"id" => 1, "name" => "General Fitness", "capacity" => 30,  "duration" => "5pm - 7pm", "premium_members" => true}
+    options = {"id" => 1, "name" => "General Fitness", "capacity" => 30,  "duration" => "2 hours", "start_time" => "2018-08-16 18:00", "premium_members" => "t"}
 
     @fitness_class = FitnessClass.new(options)
   end
@@ -26,12 +26,17 @@ class TestFitnessClass < MiniTest::Test
 
   def test_duration()
     result = @fitness_class.duration()
-    assert_equal("5pm - 7pm", result)
+    assert_equal("2 hours", result)
   end
 
   def test_premium_members()
     result = @fitness_class.premium_members()
     assert_equal(true, result)
+  end
+
+  def test_start_time()
+    result = @fitness_class.start_time()
+    assert_equal("2018-08-16 18:00", result)
   end
 
 end
