3dbb18d15ef469e3417c55b4e48dc73035c231b4
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-16 13:03:03 +0000

    further db fixes

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      PG.connect( {dbname: 'd5pscfpdpev5ih',
+      db = PG.connect( {dbname: 'd5pscfpdpev5ih',
           host: 'ec2-54-243-147-162.compute-1.amazonaws.com',
           port: 5432, user: 'mkrbocnfbivxyo', password: '7b416faf1ed5ea1d1c9be93669b4a658a76fd33fc3a7cd08653274b68416911f'})
       # local DB connection
