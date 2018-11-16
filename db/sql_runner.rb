c4c2d00bc9d4d53618b96563e0009bc3bd0de938
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-11-16 10:17:00 +0000

    amends SQL runner for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,11 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'boxing_gym', host: 'localhost' })
+      PG.connect( {dbname: 'd5pscfpdpev5ih',
+          host: 'ec2-54-243-147-162.compute-1.amazonaws.com',
+          port: 5432, user: 'mkrbocnfbivxyo', password: '7b416faf1ed5ea1d1c9be93669b4a658a76fd33fc3a7cd08653274b68416911f'})
+      # local DB connection
+      # db = PG.connect({ dbname: 'boxing_gym', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
@@ -12,5 +16,6 @@ class SqlRunner
     end
     return result
   end
+
 
 end
